a64d43282fb456bf7f9afb5c910afc6da87748a0
mcosnta000 <mconsta000@gmail.com>
2019-05-10 13:34:23 -0400

    Passing submission with improved performance

--- lru-cache/src/main/java/org/mconsta000/lrucache/LRUCache.java
+++ lru-cache/src/main/java/org/mconsta000/lrucache/LRUCache.java
@@ -1,41 +1,131 @@
 package org.mconsta000.lrucache;
 
 import java.util.HashMap;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
+import java.util.Map;
 
 /**
  * LRU Cache implimentation
  */
 public final class LRUCache {
-    private BlockingQueue<Integer> queue = null;
-    private HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+    private Map<Integer,Node> nodes = new HashMap<Integer, Node>();
+    private Node head = null;
+    private Node tail = null;
     private int capacity = 0;
-    
+    private int size = 0;
+ 
+    private class Node {
+        private Integer key;
+        private Integer value;
+        private Node previous;
+        private Node next;
+
+        Node(Integer key, Integer value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
     public LRUCache(int capacity) {
         this.capacity = capacity;
-        queue = new ArrayBlockingQueue<Integer>(this.capacity);
     }
-    
+
     public int get(int key) {
-        Integer ret = map.get(key);
-        if (ret == null)
-            ret = -1;
-        else {
-            queue.remove(key);
-            queue.offer(key);
+        Node node = nodes.get(key);
+        int ret = -1;
+        if (node != null) {
+            remove(node);
+            enqueue(node);
+            ret = node.value;
         }
-        return ret; 
+        return ret;
     }
     
     public void put(int key, int value) {
-        if (queue.contains(key)) {
-            queue.remove(key);
-        } else if (queue.size() == capacity) {
-            Integer lru = queue.poll();
-            map.remove(lru);
+        Node node = nodes.get(key);
+        if (node != null) {
+            node.value = value;
+            remove(node);
+            enqueue(node);
+        } else {
+            node = new Node(key, value);
+            if (capacity == size) {
+                nodes.remove(tail.key);
+                remove(tail);
+            }
+            enqueue(node);
+            nodes.put(node.key, node);
         }
-        map.put(key, value);
-        queue.offer(key);
+
+    }
+
+    void remove(Node node) {
+        if (node != null) {
+            if (node.next != null) {
+                node.next.previous = node.previous;
+            } else {
+                tail = node.previous;
+            }
+
+            if (node.previous != null) {
+                node.previous.next = node.next;
+            } else {
+                head = node.next;
+            }
+
+            node.next = null;
+            node.previous = null;
+            
+            size--;
+        }
+    }
+
+    void enqueue(Node node) {
+        if (node != null) {
+            if (head == null && tail == null) {
+                head = node;
+                tail = node;
+            } else  {
+                node.next = head;
+                head.previous = node;
+                head = node;
+            }
+            size++;
+        }
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int[][] getAscending() {
+        int ret[][] = new int[size][2];
+
+        Node current = head;
+        int i=0;
+        while (current != null){
+            ret[i][0] = current.key;
+            ret[i][1] = current.value;
+            i++;
+            current = current.next;
+        }
+        return ret;
+    }
+
+    public int[][] getDescending() {
+        int ret[][] = new int[size][2];
+
+        Node current = tail;
+        int i=0;
+        while (current != null){
+            ret[i][0] = current.key;
+            ret[i][1] = current.value;
+            i++;
+            current = current.previous;
+        }
+        return ret;
     }
 }
--- lru-cache/src/test/java/org/mconsta000/lrucache/LRUCacheTest.java
+++ lru-cache/src/test/java/org/mconsta000/lrucache/LRUCacheTest.java
@@ -25,4 +25,108 @@ public class LRUCacheTest {
         assertEquals(3, cache.get(3));       // returns 3
         assertEquals(4, cache.get(4));       // returns 4
     }
+
+    @Test
+    public void testAscending() {
+        LRUCache cache = new LRUCache(3);
+
+        int data[][] = {{1,1},{2,2},{3,3}};
+
+        for (int i=0; i<data.length; i++){
+            cache.put(data[i][0], data[i][1]);
+        }
+
+        int vals[][] = cache.getAscending();
+
+        for (int i=0, j=data.length-1; i<vals.length; i++, j--){
+            assertEquals(data[j][0], vals[i][0]);
+            assertEquals(data[j][1], vals[i][1]);
+        }
+    }
+
+    @Test
+    public void testDescending() {
+        LRUCache cache = new LRUCache(3);
+
+        int data[][] = {{1,1},{2,2},{3,3}};
+
+        for (int i=0; i<data.length; i++){
+            cache.put(data[i][0], data[i][1]);
+        }
+
+        int vals[][] = cache.getDescending();
+
+        for (int i=0,j=0; i<vals.length; i++, j++){
+            assertEquals(data[j][0], vals[i][0]);
+            assertEquals(data[j][1], vals[i][1]);
+        }
+    }
+
+    @Test
+    public void testUpdate() {
+        LRUCache cache = new LRUCache(3);
+
+        cache.put(1,1);
+
+        assertEquals(1, cache.get(1));
+
+        cache.put(1, 2);
+
+        assertEquals(2, cache.get(1));
+    }
+
+    @Test
+    public void testCapacity() {
+        LRUCache cache = new LRUCache(2);
+
+        cache.put(1,1);
+
+        assertEquals(1, cache.get(1));
+
+        cache.put(2, 2);
+
+        assertEquals(2, cache.get(2));
+
+        cache.put(3, 3);
+
+        assertEquals(-1, cache.get(1));
+        assertEquals(2, cache.get(2));
+        assertEquals(3, cache.get(3));
+
+        cache.put(4,4);
+
+        assertEquals(-1, cache.get(1));
+        assertEquals(-1, cache.get(2));
+        assertEquals(3, cache.get(3));
+        assertEquals(4, cache.get(4));
+    }
+
+    @Test
+    public void testLRUCache2() {
+        LRUCache cache = new LRUCache( 1000 /* capacity */ );
+
+        for (int i=0; i<2000; i++) {
+            cache.put(i,i);
+        }
+
+        assertEquals(-1, cache.get(2));       // returns -1 (not found)
+
+        cache.put(1500, 3000);
+
+        assertEquals(3000, cache.get(1500)); 
+
+        cache.put(2000, 0);
+
+        assertEquals(0, cache.get(2000)); 
+    }   
+
+    // Input
+    // ["LRUCache","put","put","put","put","put","get","put","get","get","put","get","put","put","put","get","put","get","get","get","get","put","put","get","get","get","put","put","get","put","get","put","get","get","get","put","put","put","get","put","get","get","put","put","get","put","put","put","put","get","put","put","get","put","put","get","put","put","put","put","put","get","put","put","get","put","get","get","get","put","get","get","put","put","put","put","get","put","put","put","put","get","get","get","put","put","put","get","put","put","put","get","put","put","put","get","get","get","put","put","put","put","get","put","put","put","put","put","put","put"]
+    // [[10],[10,13],[3,17],[6,11],[10,5],[9,10],[13],[2,19],[2],[3],[5,25],[8],[9,22],[5,5],[1,30],[11],[9,12],[7],[5],[8],[9],[4,30],[9,3],[9],[10],[10],[6,14],[3,1],[3],[10,11],[8],[2,14],[1],[5],[4],[11,4],[12,24],[5,18],[13],[7,23],[8],[12],[3,27],[2,12],[5],[2,9],[13,4],[8,18],[1,7],[6],[9,29],[8,21],[5],[6,30],[1,12],[10],[4,15],[7,22],[11,26],[8,17],[9,29],[5],[3,4],[11,30],[12],[4,29],[3],[9],[6],[3,4],[1],[10],[3,29],[10,28],[1,20],[11,13],[3],[3,12],[3,8],[10,9],[3,26],[8],[7],[5],[13,17],[2,27],[11,15],[12],[9,19],[2,15],[3,16],[1],[12,17],[9,1],[6,19],[4],[5],[5],[8,1],[11,7],[5,2],[9,28],[1],[2,2],[7,4],[4,22],[7,24],[9,26],[13,28],[11,26]]
+
+    /*
+      [null,null,null,null,null,null,-1,null,19,17,null,-1,null,null,null,-1,null,-1,5,-1,12,null,null,3,5,5,null,null,1,null,-1,null,30,5,30,null,null,null,-1,null,-1,24,null,null,18,null,null,null,null,14,null,null,18,null,null,11,null,null,null,null,null,18,null,null,24,null,4,29,30,null,12,11,null,null,null,null,29,null,null,null,null,17,22,18,null,null,null,24,null,null,null,20,null,null,null,29,18,18,null,null,null,null,20,null,null,null,null,null,null,null]
+      [null,null,null,null,null,null,-1,null,19,17,null,-1,null,null,null,-1,null,-1,5,-1,12,null,null,3,5,5,null,null,1,null,-1,null,30,5,30,null,null,null,-1,null,-1,24,null,null,18,null,null,null,null,-1,null,null,18,null,null,-1,null,null,null,null,null,18,null,null,-1,null,4,29,30,null,12,-1,null,null,null,null,29,null,null,null,null,17,22,18,null,null,null,-1,null,null,null,20,null,null,null,-1,18,18,null,null,null,null,20,null,null,null,null,null,null,null]
+    */
+
 }
